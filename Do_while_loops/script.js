c1cc31ccf4604112aae2deac9141e8812dfcd8e5
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-04-21 16:58:33 +0100

    Add files via upload

---
+++ Do_while_loops/script.js
@@ -0,0 +1,9 @@
+// Do While Loops // 
+
+let arr = ["milk", "bread", "cheese", "butter",];
+let count = 0; // This code counts how many times this loop will repeat //
+do {
+console.log(arr[count++]);
+} while (count < arr.length);
+
+// Not used that often but there are times when a Do While Loop is needed >>> But in general a While Loop will suffice. //
